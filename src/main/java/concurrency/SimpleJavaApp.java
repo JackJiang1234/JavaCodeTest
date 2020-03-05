21661408e51f129078df69af25d2aa6dff92cf12
Jack <jjy_8888@163.com>
2020-03-05 14:23:06 +0800

    add test

--- src/main/java/concurrency/SimpleJavaApp.java
+++ src/main/java/concurrency/SimpleJavaApp.java
@@ -1,5 +1,6 @@
 package concurrency;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -18,6 +19,7 @@ public class SimpleJavaApp {
 
         System.out.println(Thread.holdsLock(lock));
 
+
         /*
         while (true){
             System.out.println(new Date());
@@ -31,5 +33,13 @@ public class SimpleJavaApp {
             要求单程序并发至少支持1000tps 预警事件 
         *
         * */
+
+        /**
+         *
+         分级发短信（demo就打印消息 ），比如 公司领导，部门主管，小组组长，组成员4级，
+         假设让你来开发个疫情预警上报程序，先通知组人员（多个人），如果有一个处理就算处理了，如果3分钟（demo 可以时间缩短）没处理，
+         就往组长发短信，如果组长3分钟没处理就往组长的上级推送。如果没处理，就按这样一级一级的往上推送消息(打印消息)。
+         *
+         * */
     }
 }
