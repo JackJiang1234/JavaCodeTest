4e377ef9a5cd46fc16a48344dd63760261c4b035
Jack <jjy_8888@163.com>
2020-03-08 21:50:07 +0800

    add test

---
+++ src/main/java/concurrency/base/thread/TaskWithException.java
@@ -0,0 +1,27 @@
+package concurrency.base.thread;
+
+import concurrency.base.util.Tools;
+
+import java.util.Random;
+
+public class TaskWithException implements Runnable {
+    final Random rnd;
+
+    public TaskWithException(Random rnd) {
+        super();
+        this.rnd = rnd;
+    }
+
+    @Override
+    public void run() {
+
+        while(true){
+            if(rnd.nextInt(100)<2){
+                throw new RuntimeException("test");
+            }
+            Tools.randomPause(50);
+        }
+
+    }
+
+}
