45fdd29e52fe8f119295a095095028778deabaea
jack <jjy_8888@163.com>
2020-07-19 15:36:21 +0800

    Create MaximumSubarray.java

---
+++ src/main/java/ds/dp/MaximumSubarray.java
@@ -0,0 +1,20 @@
+package ds.dp;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int maxSum = sum;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (sum > 0) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+}
